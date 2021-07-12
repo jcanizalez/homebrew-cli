5855c9087a71b647a5f06d4018e6aa0a0be81989
Javier Canizalez <javier-canizalez@outlook.com>
2021-07-12 17:18:45 -0600

    Update sha256 (#3)

--- Formula/azb.rb
+++ Formula/azb.rb
@@ -2,7 +2,7 @@ class Azb < Formula
     desc "azb command line tool"
     homepage "https://github.com/AzBuilder/azb-cli"
     url "https://github.com/AzBuilder/azb-cli/releases/download/v0.1.0-alfa.2/azb-v0.1.0-alfa.2-darwin-amd64.tar.gz"
-    sha256 "babaf610ed70c8a0e78d6c64826ee88e7b31a4780d9165bea694de9b1f43669c"
+    sha256 "40b1dfc60e95a8a28576dcd79380680563ce6d73bbadabc5fcb71dead9a5528c"
     license "Apache-2.0"
   
   
