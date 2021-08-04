40a7d0a9adf4877b96680d40d74659bba239d8a9
Javier Canizalez <javier-canizalez@outlook.com>
2021-08-04 12:12:57 -0600

    Update azb.rb

--- Formula/azb.rb
+++ Formula/azb.rb
@@ -1,7 +1,7 @@
 class Azb < Formula
     desc "azb command line tool"
-    homepage "https://github.com/AzBuilder/azb-cli"
-    url "https://github.com/AzBuilder/azb-cli/releases/download/v0.1.0-alfa.2/azb-v0.1.0-alfa.2-darwin-amd64.tar.gz"
+    homepage "https://github.com/jcanizalez/azb-cli"
+    url "https://github.com/jcanizalez/azb-cli/releases/download/v2.2.0/azb-v2.2.0-darwin-amd64.tar.gz"
     sha256 "40b1dfc60e95a8a28576dcd79380680563ce6d73bbadabc5fcb71dead9a5528c"
     license "Apache-2.0"
   
